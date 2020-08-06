80b8d1b4276be8e556dea8ecb49c90a44ec454b2
Ivan Stickmanov <stickman9000@mail.ru>
2020-08-07 02:09:25 +0300

    Update project's description

--- scripts/projectsList.js
+++ scripts/projectsList.js
@@ -15,7 +15,7 @@ let projectsList = [
     { title: 'JS Tabs', image: 'projectjs.png', html: true, scss: true, js: true, link: 'https://github.com/Thundiverter/js-tabs' },
     { title: 'Thundiverter.github.io', image: 'project3.png', desc: 'My website. The one you are currently watching', html: true, scss: true, js: true, vue: true,
         link: 'https://github.com/Thundiverter/thundiverter.github.io', pin: true },
-    { title: 'Minecraft Quick Guide', image: 'project2.png', desc: 'All potion recipes and enchantments in a table', html: true, scss: true,
+    { title: 'Minecraft Quick Guide', image: 'project2.png', desc: 'All potion recipes in a table', html: true, scss: true,
         link: 'https://github.com/Thundiverter/minecraft-quick-guide', pin: true },
     { title: 'Box-shadow Generator', image: 'project1.png', desc: 'Visual CSS box-shadow generator', html: true, scss: true, js: true,
         link: 'https://github.com/Thundiverter/box-shadow-generator', pin: true }
